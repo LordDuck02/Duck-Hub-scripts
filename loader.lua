4ddbf41ca59391e0457e93bb5dd5c438e917b27d
1lordduck <97839276+LordDuck02@users.noreply.github.com>
2024-05-05 20:50:36 +0000

    added kaiju paradise support

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 loadstring(({
 [13132367906]="https://linktoloadstring/",
-[6456351776]="https://linktoloadstring/"
+[6456351776]="https://raw.githubusercontent.com/LordDuck02/Duck-Hub-scripts/main/scripts/Kaiju%20paradise.lua"
 })[game.PlaceId] or "print('game not supported')")()
