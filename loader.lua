cae3436d3447bdeb180e73c933c4788131358bb1
1lordduck <97839276+LordDuck02@users.noreply.github.com>
2024-05-05 20:56:52 +0000

    added proper loadstrings

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 loadstring(({
-[13132367906]="https://linktoloadstring/",
-[6456351776]="https://raw.githubusercontent.com/LordDuck02/Duck-Hub-scripts/main/scripts/Kaiju%20paradise.lua"
+    [13132367906] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LordDuck02/Duck-Hub-scripts/main/scripts/EB.lua'))()",
+    [6456351776] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LordDuck02/Duck-Hub-scripts/main/scripts/Kaiju%20paradise.lua'))()"
 })[game.PlaceId] or "print('game not supported')")()
