beac656ba696ee18a268efd5da5c321f11b8a7b2
1lordduck <lordgabrielduck@gmail.com>
2024-05-05 17:41:25 -0300

    Your descriptive commit message here

---
+++ scripts/EB.lua
@@ -0,0 +1,127 @@
+local plrs = game:GetService("Players")
+local cc = workspace.CurrentCamera
+local lp = plrs.LocalPlayer
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local mainW = OrionLib:MakeWindow({Name = "Duck Hub - EB do Tevez", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local mainTab  = mainW:MakeTab({
+    Name = "Geral",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+    
+mainTab:AddSection({
+    Name = "Geral"
+})
+
+mainTab:AddSlider({
+	Name = "FOV",
+	Min = 0,
+	Max = 120,
+	Default = 70,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "Valor",
+	Callback = function(Value)
+		cc.FieldOfView = Value
+	end    
+})
+
+mainTab:AddSlider({
+    Name = "Tamanho da hitbox",
+    Min = 2,
+    Max = 150,
+    Default = 2,
+    Increment = 1,
+    ValueName = "Tamanho",
+    Color = Color3.fromRGB(255, 255, 255),
+    Callback = function(value)
+        _G.HeadSize = value
+        _G.Disabled = true
+        
+        if _G.RenderConnection then
+            _G.RenderConnection:Disconnect()
+        end
+        
+        _G.RenderConnection = game:GetService("RunService").RenderStepped:Connect(function()
+            if _G.Disabled then
+                for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+                    if v.Name ~= game:GetService("Players").LocalPlayer.Name then
+                        pcall(function()
+                            local humanoidRootPart = v.Character and v.Character:FindFirstChild("HumanoidRootPart")
+                            if humanoidRootPart then
+                                humanoidRootPart.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
+                                humanoidRootPart.Transparency = 0.7
+                                humanoidRootPart.BrickColor = BrickColor.new("Really blue")
+                                humanoidRootPart.Material = "Neon"
+                                humanoidRootPart.CanCollide = false
+                            end
+                        end)
+                    end
+                end
+            end
+        end)
+    end
+})
+
+local trollTab = mainW:MakeTab({
+    Name = "Troll",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Section = trollTab:AddSection({
+	Name = "Troll"
+})
+
+trollTab:AddButton({
+	Name = "Icone mobile (ilimitado)",
+	Callback = function()
+      		game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Device"):FireServer("Mobile")
+  	end    
+})
+
+trollTab:AddButton({
+	Name = "Icone PC (ilimitado)",
+	Callback = function()
+      		game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Device"):FireServer("Computer")
+  	end    
+})
+
+trollTab:AddButton({
+    Name = "Fake AFK",
+    Callback = function()
+        game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("AFK"):FireServer(true)
+    end
+})
+
+local lojaTab  = mainW:MakeTab({
+    Name = "Loja",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+lojaTab:AddSection({
+    Name = "Armas"
+})
+
+local farmTab  = mainW:MakeTab({
+    Name = "Farm",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+farmTab:AddSection({
+    Name = "Auto-Farm"
+})
+
+farmTab:AddButton({
+    Name = "Vender dinheiro roubado",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Assets.Remotes.Robbery:FireServer("Payment")
+    end
+})
+
+
+OrionLib:Init()
+
---
+++ scripts/Kaiju paradise.lua
@@ -0,0 +1,85 @@
+local ws = game:GetService("Workspace")
+local plrs = game:GetService("Players")
+local lp = plrs.LocalPlayer
+local Orion = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local itemSpots = {"KnifeSpot", "KatanaSpot", "ScytheSpot", "TaserSpot"}
+local foodSpots = {}
+
+local checkChild = false
+
+
+local function addUI(part)
+    local partGui = Instance.new("BillboardGui", part)
+    partGui.Size = UDim2.new(2, 0, 2, 0)
+    partGui.AlwaysOnTop = true
+    partGui.Name = "Item ESP"
+    local frame = Instance.new("Frame", partGui)
+    frame.BackgroundColor3 = Color3.fromRGB(0, 0, 150)  
+    frame.BackgroundTransparency = 1
+    frame.Size = UDim2.new(1.3, 0, 1.3, 0)
+    frame.BorderSizePixel = 0 
+
+    local nameGUI = Instance.new("BillboardGui", part)
+    nameGUI.Size = UDim2.new(3, 0, 1.5, 0)
+    nameGUI.SizeOffset = Vector2.new(0, 1)
+    nameGUI.AlwaysOnTop = true
+    nameGUI.Name = "Name"
+
+    local text = Instance.new("TextLabel", nameGUI)
+    text.Font = Enum.Font.GothamSemibold
+    text.Name = "Text"
+    text.Text = part.Name
+    text.TextColor3 = Color3.fromRGB(255, 80, 60)
+    text.Transparency = 0
+    text.BackgroundTransparency = 1
+    text.TextScaled = true
+    text.Size = UDim2.new(1, 0, 1, 0)
+end
+
+local function itemESP()
+    for _, child in ipairs(ws.Scripted.ItemSpawner:GetChildren()) do
+        if table.find(itemSpots, child.Name) then
+            for _, item in ipairs(child:GetChildren()) do 
+                addUI(item)
+                Instance.new("Highlight", item)
+            end
+        end
+    end
+end
+
+local function allESP()
+    for _, player in ipairs(plrs:GetChildren()) do 
+            addUI(player)
+        end
+end
+
+local Window = Orion:MakeWindow({Name = "Kaiju Paradise - Duck hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local VisualTab = Window:MakeTab({
+    Name = "Visuals",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+VisualTab:AddToggle({
+    Name = "ESP",
+    Default = false,
+    Callback = allESP()
+})
+
+VisualTab:AddToggle({
+    Name = "Item ESP",
+    Default = false,
+    Callback = itemESP()
+})
+
+local Section = VisualTab:AddSection({
+	Name = "Event items"
+})
+
+local ItemsTab = Window:MakeTab({
+    Name = "Items",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
