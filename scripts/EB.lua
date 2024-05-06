75e39971bbfdb81b26fa0342cb041b3d7f6924cb
1lordduck <97839276+LordDuck02@users.noreply.github.com>
2024-05-06 18:52:06 +0000

    new functions and stuff

--- scripts/EB.lua
+++ scripts/EB.lua
@@ -1,69 +1,102 @@
-print("Loading") -- Useless
-local plrs = game:GetService("Players")
-local cc = workspace.CurrentCamera
+-- findfirstchildofclass é mais rapido que getservice
+local game = game -- otimização de 0.1ms
+local ws = game:FindFirstChildOfClass("Workspace")
+local plrs = game:FindFirstChildOfClass("Players")
+local cplrs = plrs:GetPlayers()
+local runService = game:FindFirstChildOfClass("RunService")
+local replicatedStorage = game:FindFirstChildOfClass("ReplicatedStorage")
+local strtGui = game:FindFirstChildOfClass("StarterGui")
+
+
+local assetsF = replicatedStorage.Assets
+local deviceR = assetsF.Remotes.Device
+local robR = assetsF.Remotes.Robbery
+local afkR = assetsF.Remotes.AFK
+local buyR =  assetsF.Remotes:WaitForChild("ToolsShop")
+local buyRR = assetsF.Remotes:WaitForChild("BuyShop")
+
+local cc = ws.CurrentCamera
 local lp = plrs.LocalPlayer
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local mainW = OrionLib:MakeWindow({Name = "Duck Hub - EB do Tevez", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local mainTab  = mainW:MakeTab({
-    Name = "Geral",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+local function notify(title, text, duration)
+    strtGui:SetCore("SendNotification", {
+        Title = title,
+        Text = text,
+        Duration = duration
+    })
+end
+
+notify("Duck Hub", "Carregando EB", 3)
+
+local HitboxExpander = {
+    settings = {
+        Transparency = 0.7,
+        HeadSize = 2,
+        RenderConnection = nil
+    }
+}
+
+local function onHitboxSizeChange(value)
+    HitboxExpander.settings.HeadSize = value
     
-mainTab:AddSection({
-    Name = "Geral"
-})
-
-mainTab:AddSlider({
-	Name = "FOV",
-	Min = 0,
-	Max = 120,
-	Default = 70,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Valor",
-	Callback = function(Value)
-		cc.FieldOfView = Value
-	end    
-})
-
-mainTab:AddSlider({
-    Name = "Tamanho da hitbox",
-    Min = 2,
-    Max = 150,
-    Default = 2,
-    Increment = 1,
-    ValueName = "Tamanho",
-    Color = Color3.fromRGB(255, 255, 255),
-    Callback = function(value)
-        _G.HeadSize = value
-        _G.Disabled = true
-        
-        if _G.RenderConnection then
-            _G.RenderConnection:Disconnect()
-        end
-        
-        _G.RenderConnection = game:GetService("RunService").RenderStepped:Connect(function()
-            if _G.Disabled then
-                for i, v in pairs(game:GetService("Players"):GetPlayers()) do
-                    if v.Name ~= game:GetService("Players").LocalPlayer.Name then
-                        pcall(function()
-                            local humanoidRootPart = v.Character and v.Character:FindFirstChild("HumanoidRootPart")
-                            if humanoidRootPart then
-                                humanoidRootPart.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
-                                humanoidRootPart.Transparency = 0.7
-                                humanoidRootPart.BrickColor = BrickColor.new("Really blue")
-                                humanoidRootPart.Material = "Neon"
-                                humanoidRootPart.CanCollide = false
-                            end
-                        end)
+    if HitboxExpander.settings.RenderConnection then
+        HitboxExpander.settings.RenderConnection:Disconnect()
+    end
+    
+    HitboxExpander.settings.RenderConnection = runService.Heartbeat:Connect(function()
+        for _, player in ipairs(cplrs) do
+            if player ~= lp then
+                local character = player.Character
+                if character then
+                    local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+                    if humanoidRootPart then
+                        humanoidRootPart.Size = Vector3.new(HitboxExpander.settings.HeadSize, HitboxExpander.settings.HeadSize, HitboxExpander.settings.HeadSize)
+                        humanoidRootPart.Transparency = HitboxExpander.settings.Transparency
+                        humanoidRootPart.BrickColor = BrickColor.new("Really blue")
+                        humanoidRootPart.Material = Enum.Material.Neon
+                        humanoidRootPart.CanCollide = false
+                    else
+                        print("HRP não encontrado: " .. player.Name)
                     end
                 end
             end
-        end)
-    end
+        end
+    end)
+end
+
+local function createSlider(tab, name, min, max, default, color, increment, valueName, callback)
+    tab:AddSlider({
+        Name = name,
+        Min = min,
+        Max = max,
+        Default = default,
+        Color = color,
+        Increment = increment,
+        ValueName = valueName,
+        Callback = callback
+    })
+end
+
+local pvpTab = mainW:MakeTab({
+    Name = "PvP",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
+
+pvpTab:AddSection({ Name = "Geral" })
+
+createSlider(pvpTab, "FOV", 0, 120, 70, Color3.fromRGB(255, 255, 255), 1, "Valor", function(value)
+    cc.FieldOfView = value
+end)
+
+createSlider(pvpTab, "Tamanho da hitbox", 2, 150, HitboxExpander.settings.HeadSize, Color3.fromRGB(255, 255, 255), 1, "Tamanho", onHitboxSizeChange)
+
+createSlider(pvpTab, "Transparência da hitbox", 0, 1, HitboxExpander.settings.Transparency, Color3.fromRGB(255, 255, 255), 0.1, "Transparência", function(value)
+    HitboxExpander.settings.Transparency = value
+end)
 
 local trollTab = mainW:MakeTab({
     Name = "Troll",
@@ -71,32 +104,28 @@ local trollTab = mainW:MakeTab({
     PremiumOnly = false
 })
 
-local Section = trollTab:AddSection({
-	Name = "Troll"
+trollTab:AddButton({
+    Name = "Icone mobile (ilimitado)",
+    Callback = function()
+        deviceR:FireServer("Mobile")
+    end    
 })
 
 trollTab:AddButton({
-	Name = "Icone mobile (ilimitado)",
-	Callback = function()
-      		game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Device"):FireServer("Mobile")
-  	end    
-})
-
-trollTab:AddButton({
-	Name = "Icone PC (ilimitado)",
-	Callback = function()
-      		game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Device"):FireServer("Computer")
-  	end    
+    Name = "Icone PC (ilimitado)",
+    Callback = function()
+        deviceR:FireServer("Computer")
+    end    
 })
 
 trollTab:AddButton({
     Name = "Fake AFK",
     Callback = function()
-        game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("AFK"):FireServer(true)
+        afkR:FireServer(true)
     end
 })
 
-local lojaTab  = mainW:MakeTab({
+local lojaTab = mainW:MakeTab({
     Name = "Loja",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
@@ -106,7 +135,67 @@ lojaTab:AddSection({
     Name = "Armas"
 })
 
-local farmTab  = mainW:MakeTab({
+lojaTab:AddButton({
+    Name = "Colete",
+    Callback = function()
+        buyR:FireServer("Buy", "Colete")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "AK-47 (15000)",
+    Callback = function()
+        buyR:FireServer("Buy", "AK-47")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "MPT-76 (8500)",
+    Callback = function()
+        buyR:FireServer("Buy", "MPT-76")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "Uzi (8200)",
+    Callback = function()
+        buyR:FireServer("Buy", "UZI")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "Glock 18 (4300)",
+    Callback = function()
+        buyR:FireServer("Buy", "GLOCK 18")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "M4A1 (13000)",
+    Callback = function()
+        buyR:FireServer("Buy", "M4A1")
+    end
+})
+
+lojaTab:AddSection({
+    Name = "Outros"
+})
+
+lojaTab:AddButton({
+    Name = "Keycard (300)",
+    Callback = function()
+        buyRR:FireServer("Keycard")
+    end
+})
+
+lojaTab:AddButton({
+    Name = "C4 (13000)",
+    Callback = function()
+        buyRR:FireServer("C4")
+    end
+})
+
+local farmTab = mainW:MakeTab({
     Name = "Farm",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
@@ -119,10 +208,8 @@ farmTab:AddSection({
 farmTab:AddButton({
     Name = "Vender dinheiro roubado",
     Callback = function()
-        game:GetService("ReplicatedStorage").Assets.Remotes.Robbery:FireServer("Payment")
+        robR:FireServer("Payment")
     end
 })
 
-
 OrionLib:Init()
-
